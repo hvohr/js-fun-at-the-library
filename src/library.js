8d67a6c5491bce8538d92c1e04243a99b485f6fa
Hollis Vohr <hollis.vohr@gmail.com>
2023-03-24 15:53:43 -0600

    Add finished createLibrary function

--- src/library.js
+++ src/library.js
@@ -1,8 +1,21 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library
+};
+
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook,
   // takeStock
 };
