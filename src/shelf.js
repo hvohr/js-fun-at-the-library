9fecc8a1db30c3ae32d751b37ee1c97dbf6c61e2
Hollis Vohr <hollis.vohr@gmail.com>
2023-03-24 11:00:16 -0600

    Add finished listTitles function

--- src/shelf.js
+++ src/shelf.js
@@ -9,12 +9,20 @@ function unshelfBook(title, shelf) {
     if (shelf[i].title === title) {
       return shelf.splice(i, 1)
   }
+} 
 }
-}
+
+function listTitles(shelf) {
+  var array = []
+  for (var i = 0; i < shelf.length; i++) {
+    array.push(shelf[i].title)
+    }
+    return array.join(', ')
+   };
 
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
+  listTitles,
   // searchShelf
 };
