ccb710d63a4dec1a4fcdbb3e0808851b96d6d5a4
Hollis Vohr <hollis.vohr@gmail.com>
2023-03-25 10:14:07 -0600

    Add finished checkoutBook function

--- src/library.js
+++ src/library.js
@@ -14,10 +14,29 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book);
 };
 
+function checkoutBook(library, bookTitle, bookGenre) {
+  var bookExist = false; // 
+  for (var i = 0; i < library.shelves[bookGenre].length; i++) {
+    if (library.shelves[bookGenre][i].title === bookTitle)
+    library.shelves[bookGenre].splice(i, 1);
+    bookExist = true;
+  }
+if (bookExist === true) {
+  return `You have now checked out ${bookTitle} from the ${library.name}.`
+} else {
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}.`
+}
+}
+
+
+
+function takeStock(library,genre){
+  return `There are a total of ${library.shelves[genre].length} ${genre} books at the ${library.name}.`
+} 
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook,
-  // takeStock
+  checkoutBook,
+  takeStock
 };
